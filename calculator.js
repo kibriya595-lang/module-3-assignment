a205d626f7f8b3fbc167a941844dd61d02af1bf2
Md Mustakim <kibriya595@gmail.com>
2026-06-06 22:30:25 +0600

    Update Calculator Program

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 
-//  Simple Calculator
+// Simple Calculator
 
 let num_1 = 10;
 let num_2 = 5;
@@ -13,15 +13,21 @@ function subtraction(a, b) {
 function multiplication(a, b) {
     return a * b;
 };
-function devition(a, b) {
+function divition(a, b) {
+    if (b === 0) {
+        return "Error: Division by zero is not allowed."
+    }
     return a / b;
 };
 function modulus(a, b) {
+    if (b === 0) {
+        return "Error: Modulus by zero is not allowed."
+    }
     return a % b;
 };
 
 console.log("Addition: " + addition(num_1, num_2));               // Addition: 15
 console.log("Subtraction: " + subtraction(num_1, num_2));         // Subtraction: 5
 console.log("Multiplication: " + multiplication(num_1, num_2));   // Multiplication: 50
-console.log("Devition: " + devition(num_1, num_2));               // Devition: 2
+console.log("Divition: " + divition(num_1, num_2));               // Divition: 2
 console.log("Modulus: " + modulus(num_1, num_2));                 // Modulus: 0
