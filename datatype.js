559e2428dec94eb02b2b61bb490be38a55bdf681
Md Mustakim <kibriya595@gmail.com>
2026-06-06 23:25:45 +0600

    Updat

--- datatype.js
+++ datatype.js
@@ -10,5 +10,5 @@ const phone = undefined;
 console.log(typeof name);        // string
 console.log(typeof age);         // number
 console.log(typeof isStudent);   // true
-console.log(typeof salary);      // null
+console.log(typeof salary);      // object
 console.log(typeof phone)        // undefined
