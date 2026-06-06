fa1e043f44dc28e045313985fd9cfa24ffe7ad6d
Md Mustakim <kibriya595@gmail.com>
2026-06-06 23:19:01 +0600

    Updata Program

--- datatype.js
+++ datatype.js
@@ -1,11 +1,11 @@
 
 // Data Types Check
 
-const name = "Md Mustakim Billah";
-const age = 20;
-const isStudent = true;
-const salary = null;
-const phone;
+let name = "Md Mustakim Billah";
+let age = 20;
+let isStudent = true;
+let salary = null;
+let phone;
 
 console.log(typeof name);        // string
 console.log(typeof age);         // number
